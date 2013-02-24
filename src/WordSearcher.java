4ba450227b26deadc0aa83380e86eae131aadf25
Jason Edstrom <agentedstrom@gmail.com>
2013-02-24 02:45:01 -0600

    WordSeacher creation

---
+++ src/WordSearcher.java
@@ -0,0 +1,26 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jason Edstrom
+ * Class: ICS 340
+ * Assignment: ICS340-PA3
+ * Date: 2/24/13
+ * Time: 2:38 AM
+ * Java Class: PACKAGE_NAME
+ */
+public class WordSearcher {
+    WordCollection miniWordCollection;
+    int grid_height = 10;
+    int grid_width = 10;
+    boolean can_overlap = true;
+    String [] [] puzzle;
+    String [] [] solution;
+
+    public WordSearcher(WordCollection some_word_collection, int a_grid_width, int a_grid_height, boolean a_can_overlap){
+
+
+    }
+
+    public WordSearcher (WordCollection some_word_collection) = new WordSearcher(some_word_collection,10,10,true);
+
+
+}
